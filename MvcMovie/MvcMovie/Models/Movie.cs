a5cea2c70b9d2656bb566634dda62a3937ffcd93
Trong Minh Tran <trminhtrong@gmail.com>
2017-09-06 10:39:39 +0700

    globalization

--- MvcMovie/MvcMovie/Models/Movie.cs
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcMovie.Models
 {
@@ -9,6 +10,9 @@ namespace MvcMovie.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        [Display(Name ="Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:d}", ApplyFormatInEditMode =true)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
