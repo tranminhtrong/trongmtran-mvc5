990b19c7725123d9a11deecf88a1769055554ea0
Trong Minh Tran <trminhtrong@gmail.com>
2017-09-10 12:22:23 +0700

    implement resiliency and command interception

---
+++ MvcMovie/ContosoUniversity/DAL/SchoolConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolConfiguration:DbConfiguration
+    {
+        public SchoolConfiguration()
+        {
+            //SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+            //To set the maximum number of retries to 1 and the maximum delay to 30 seconds
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy(1, TimeSpan.FromSeconds(30)));
+        }
+    }
+}
